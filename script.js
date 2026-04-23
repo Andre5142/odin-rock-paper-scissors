4c67ce3c520bda78fe3d3095100fe3e2e77b4842
Andre5142 <243345333+Andre5142@users.noreply.github.com>
2026-04-23 10:35:59 +0200

    New selection functionality
    Added selection image for the player's choice
    added replacement of said image on other choice selection

--- script.js
+++ script.js
@@ -22,10 +22,78 @@ let five = false
 let seven = false
 let warnText = false
 
+let rockChoice = false
+let paperChoice = false
+let scissorsChoice = false
+
+
 //Elements________________________________________________________________________________________________________
 
 const mainBox = document.querySelector("#mainBox")
 const playButton = document.createElement("button")
+
+//StartMenu
+const roundChoice = document.createElement("div")
+const roundThree = document.createElement("button")
+const roundFive = document.createElement("button")
+const roundSeven = document.createElement("button")
+const gameStartBox = document.createElement("div")
+const gameStart = document.createElement("button")
+
+//ScoreBoard
+const scoreBoardBox = document.createElement("div")
+const scoreText = document.createElement("span")
+const scorePointOne = document.createElement("div")
+const scorePointTwo = document.createElement("div")
+const scorePointThree = document.createElement("div")
+const scorePointFour = document.createElement("div")
+const scorePointFive = document.createElement("div")
+const scorePointSix = document.createElement("div")
+const scorePointSeven = document.createElement("div")
+
+//PlayField
+const playField = document.createElement("div")
+const playFieldPlayerOneBox = document.createElement("div")
+const playFieldPlayerTwoBox = document.createElement("div")
+const playFieldPlayerOneText = document.createElement("span")
+const playFieldPlayerTwoText = document.createElement("span")
+const playFieldPlayerOne = document.createElement("div")
+const playFieldPlayerTwo = document.createElement("div")
+
+const selectChoiceBox = document.createElement("div")
+const selectChoiceRock = document.createElement("button")
+const selectChoicePaper = document.createElement("button")
+const selectChoiceScissors = document.createElement("button")
+
+//sprites
+//choice
+const rockSprite = document.createElement("img")
+const paperSprite = document.createElement("img")
+const scissorsSprite = document.createElement("img")
+rockSprite.src = "./icons/stone-96.png"
+paperSprite.src = "./icons/paper-48.png"
+scissorsSprite.src = "./icons/scissors-64.png"
+
+//player
+const rockSpritePlayer = document.createElement("img")
+const paperSpritePlayer = document.createElement("img")
+const scissorsSpritePlayer = document.createElement("img")
+rockSpritePlayer.src = "./icons/stone-96.png"
+paperSpritePlayer.src = "./icons/paper-48.png"
+scissorsSpritePlayer.src = "./icons/scissors-64.png"
+
+//comp
+const rockSpriteComp = document.createElement("img")
+const paperSpriteComp = document.createElement("img")
+const scissorsSpriteComp = document.createElement("img")
+rockSpriteComp.src = "./icons/stone-96.png"
+paperSpriteComp.src = "./icons/paper-48.png"
+scissorsSpriteComp.src = "./icons/scissors-64.png"
+
+rockSpritePlayer.id = "playerIcon"
+paperSpritePlayer.id = "playerIcon"
+scissorsSpritePlayer.id = "playerIcon"
+
 
 playButton.id = "playButton"
 
@@ -89,13 +157,6 @@ mainBox.appendChild(playButton)
 
 playButton.addEventListener("click", (event) => {
     event.preventDefault()
-
-    const roundChoice = document.createElement("div")
-    const roundThree = document.createElement("button")
-    const roundFive = document.createElement("button")
-    const roundSeven = document.createElement("button")
-    const gameStartBox = document.createElement("div")
-    const gameStart = document.createElement("button")
 
     //Start_ID/Class________________________________________________________________________________________________
 
@@ -219,16 +280,6 @@ function scoreBoard() {
 
     //Scoreboard__________________________________________________________________________________________________
 
-    const scoreBoardBox = document.createElement("div")
-    const scoreText = document.createElement("span")
-    const scorePointOne = document.createElement("div")
-    const scorePointTwo = document.createElement("div")
-    const scorePointThree = document.createElement("div")
-    const scorePointFour = document.createElement("div")
-    const scorePointFive = document.createElement("div")
-    const scorePointSix = document.createElement("div")
-    const scorePointSeven = document.createElement("div")
-
     scoreBoardBox.id = "scoreBoardBox"
     scoreText.id = "scoreText"
     scorePointOne.className = "scorePoint"
@@ -239,13 +290,6 @@ function scoreBoard() {
     scorePointSix.className = "scorePoint"
     scorePointSeven.className = "scorePoint"
 
-    scorePointOne.textContent = ""
-    scorePointTwo.textContent = ""
-    scorePointThree.textContent = ""
-    scorePointFour.textContent = ""
-    scorePointFive.textContent = ""
-    scorePointSix.textContent = ""
-    scorePointSeven.textContent = ""
     scoreText.textContent = "SCORE"
     mainBox.appendChild(scoreText)
     mainBox.appendChild(scoreBoardBox)
@@ -270,21 +314,13 @@ function scoreBoard() {
 //Playing_field_____________________________________________________________________________________________________
 
 function playingField() {
-    const playField = document.createElement("div")
-    const playFieldPlayerOneBox = document.createElement("div")
-    const playFieldPlayerTwoBox = document.createElement("div")
-    const playFieldPlayerOneText = document.createElement("span")
-    const playFieldPlayerTwoText = document.createElement("span")
-    const playFieldPlayerOne = document.createElement("div")
-    const playFieldPlayerTwo = document.createElement("div")
-
-    const selectChoiceBox = document.createElement("div")
-    const selectChoiceRock = document.createElement("button")
-    const selectChoicePaper = document.createElement("button")
-    const selectChoiceScissors = document.createElement("button")
 
     playFieldPlayerOneText.textContent = "You"
     playFieldPlayerTwoText.textContent = "ComPlayer"
+
+    selectChoiceRock.appendChild(rockSprite)
+    selectChoicePaper.appendChild(paperSprite)
+    selectChoiceScissors.appendChild(scissorsSprite)
 
     playField.id = "playField"
     playFieldPlayerOneBox.id = "playFieldPlayerBox"
@@ -310,5 +346,90 @@ function playingField() {
     selectChoiceBox.appendChild(selectChoicePaper)
     selectChoiceBox.appendChild(selectChoiceScissors)
 
+    gameChoices()
+}
 
+function gameChoices() {
+
+    selectChoiceRock.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        selectChoiceRock.id = "gameChoiceSelected"
+        selectChoicePaper.id = "gameChoiceNot"
+        selectChoiceScissors.id = "gameChoiceNot"
+
+        gameChoiceRemover("rock")
+        gameChoiceSelector("rock")
+    })
+    selectChoicePaper.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        selectChoiceRock.id = "gameChoiceNot"
+        selectChoicePaper.id = "gameChoiceSelected"
+        selectChoiceScissors.id = "gameChoiceNot"
+
+        gameChoiceRemover("paper")
+        gameChoiceSelector("paper")
+    })
+
+    selectChoiceScissors.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        selectChoiceRock.id = "gameChoiceNot"
+        selectChoicePaper.id = "gameChoiceNot"
+        selectChoiceScissors.id = "gameChoiceSelected"
+
+        gameChoiceRemover("scissors")
+        gameChoiceSelector("scissors")
+    })
+
+}
+
+
+function gameChoiceSelector(choice) {
+
+    if (choice == "rock") {
+        rockChoice = true
+        paperChoice = false
+        scissorsChoice = false
+
+        playFieldPlayerOne.appendChild(rockSpritePlayer)
+    }
+    else if (choice == "paper") {
+        rockChoice = false
+        paperChoice = true
+        scissorsChoice = false
+       
+        playFieldPlayerOne.appendChild(paperSpritePlayer)
+    }
+    else {
+        rockChoice = false
+        paperChoice = false
+        scissorsChoice = true
+
+        playFieldPlayerOne.appendChild(scissorsSpritePlayer)
+    }
+}
+
+function gameChoiceRemover(keep) {
+
+    if (keep == "rock" && paperChoice == true) {
+        playFieldPlayerOne.removeChild(paperSpritePlayer)
+    }
+    else if (keep == "rock" && scissorsChoice == true) {
+        playFieldPlayerOne.removeChild(scissorsSpritePlayer)
+    }
+    else if (keep == "paper" && rockChoice == true) {
+        playFieldPlayerOne.removeChild(rockSpritePlayer)
+    }
+    else if (keep == "paper" && scissorsChoice == true) {
+        playFieldPlayerOne.removeChild(scissorsSpritePlayer)
+    }
+    else if (keep == "scissors" && rockChoice == true) {
+        playFieldPlayerOne.removeChild(rockSpritePlayer)
+    }
+    else if (keep == "scissors" && paperChoice == true) {
+        playFieldPlayerOne.removeChild(paperSpritePlayer)
+    }
+    else{}
 }
